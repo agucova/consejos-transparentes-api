abb2eb10245e39dda763f3a1c82b66be18ff28ab
Agustín Covarrubias <agucova@uc.cl>
2020-08-10 22:08:34 -0400

    Add CORS towards Github

--- main.py
+++ main.py
@@ -3,9 +3,26 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from model import Asistencias, Representante, Session
 from tasks import actualizar_db, as_dict
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = [
+    "https://agucova.github.io",
+    "http://localhost",
+    "http://127.0.0.1",
+    "http://127.0.0.1:5500",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 actualizar_db()
 
