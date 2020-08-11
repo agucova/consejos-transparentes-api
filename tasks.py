045006d8b434a1cfdb4d73fee4921bc785fda2bd
Agustín Covarrubias <agucova@uc.cl>
2020-08-10 22:33:04 -0400

    Move credentials to gunicorn's runtime env

--- tasks.py
+++ tasks.py
@@ -14,6 +14,7 @@ from celery import Celery
 from celery.schedules import solar
 import datetime as dt
 import pandas as pd
+from sheets import get_generational, get_academic, setup_service
 
 
 queue = Celery("tasks", broker="redis://localhost:6379/0")
@@ -72,41 +73,41 @@ def transformar_representaciones(representaciones):
 @queue.task
 def actualizar_db():
     session = Session()
-    # service = setup_service()
-    # generacional = get_generational(
-    #     service, "1USCGq_T6GfEC2662Z6iRrSzc1AAoMHM4W13ATAvFs54"
-    # )
-    generacional = [
-        ["Representación", "Nombre", "7/5/20", "5/6/20", "1/7/20", "4/7/20", "06/8/20"],
-        ["2020", "Agustín Covarrubias", "P", "P", "P", "P", "P"],
-        ["2020", "Katherine Catoni", "P", "P", "P", "P", "P"],
-        ["2020", "Rafael Rencoret", "P", "P", "P", "P", "P"],
-        ["2019", "Tania Hinostroza", "P", "P", "P", "P", "P"],
-        ["2019", "Florencia Sciaraffia", "P", "P", "P", "P", "P"],
-        ["2019", "Martín Illanes", "P", "P", "P", "P", "P"],
-        ["2018", "Joaquín Castaños", "P", "P", "J", "P", "P"],
-        ["2018", "Elizabeth Hermosilla", "P", "P", "J", "P", "P"],
-        ["2018", "Pedro Becker", "P", "P", "P", "P", "P"],
-        ["2017", "Camila López", "P", "P", "P", "P", "P"],
-        ["2017", "Francisco Úrzua", "P", "P", "P", "P", "O"],
-        ["2017", "Bartolomé Peirano", "P", "P", "P", "P", "P"],
-        ["2016", "Manuel Jara", "P", "P", "P", "P", "P"],
-        ["2016", "Ivania Arias", "P", "P", "P", "P", "P"],
-        ["2016", "María Belén Echenique", "P", "P", "P", "P", "P"],
-        ["2015 y ant.", "Denise Cariaga", "P", "P", "P", "P", "P"],
-        ["2015 y ant.", "Graciela Hernández", "P", "P", "P", "P", "P"],
-        ["2015 y ant.", "Caterin Pinto", "P", "P", "P", "P", "O"],
-        ["CAI", "Isa Oyarzo", "P", "P", "P", "P", "P"],
-        ["CAI", "Thomas Struszer", "P", "P", "P", "P", "P"],
-        ["CAI", "Agustín Cox", "P", "P", "P", "P", "P"],
-        ["CAI", "Claudio Escobar", "P", "P", "P", "P", "P"],
-        ["CAI", "Javiera Dawabe", "P", "P", "P", "P", "P"],
-        ["CT", "José Pereira", "P", "P", "P", "A", "P"],
-        ["CT", "Trinidad Larraín", "P", "P", "P", "P", "A"],
-        ["CT", "María Ignacia Henriquez", "P", "P", "P", "J", "P"],
-        ["CT", "Tomás Álvarez", "A", "A", "A", "A", "A"],
-        ["CT", "Magdalena Merino", "P", "P", "P", "P", "P"],
-    ]
+    service = setup_service()
+    generacional = get_generational(
+        service, "1USCGq_T6GfEC2662Z6iRrSzc1AAoMHM4W13ATAvFs54"
+    )
+    # generacional = [
+    #     ["Representación", "Nombre", "7/5/20", "5/6/20", "1/7/20", "4/7/20", "06/8/20"],
+    #     ["2020", "Agustín Covarrubias", "P", "P", "P", "P", "P"],
+    #     ["2020", "Katherine Catoni", "P", "P", "P", "P", "P"],
+    #     ["2020", "Rafael Rencoret", "P", "P", "P", "P", "P"],
+    #     ["2019", "Tania Hinostroza", "P", "P", "P", "P", "P"],
+    #     ["2019", "Florencia Sciaraffia", "P", "P", "P", "P", "P"],
+    #     ["2019", "Martín Illanes", "P", "P", "P", "P", "P"],
+    #     ["2018", "Joaquín Castaños", "P", "P", "J", "P", "P"],
+    #     ["2018", "Elizabeth Hermosilla", "P", "P", "J", "P", "P"],
+    #     ["2018", "Pedro Becker", "P", "P", "P", "P", "P"],
+    #     ["2017", "Camila López", "P", "P", "P", "P", "P"],
+    #     ["2017", "Francisco Úrzua", "P", "P", "P", "P", "O"],
+    #     ["2017", "Bartolomé Peirano", "P", "P", "P", "P", "P"],
+    #     ["2016", "Manuel Jara", "P", "P", "P", "P", "P"],
+    #     ["2016", "Ivania Arias", "P", "P", "P", "P", "P"],
+    #     ["2016", "María Belén Echenique", "P", "P", "P", "P", "P"],
+    #     ["2015 y ant.", "Denise Cariaga", "P", "P", "P", "P", "P"],
+    #     ["2015 y ant.", "Graciela Hernández", "P", "P", "P", "P", "P"],
+    #     ["2015 y ant.", "Caterin Pinto", "P", "P", "P", "P", "O"],
+    #     ["CAI", "Isa Oyarzo", "P", "P", "P", "P", "P"],
+    #     ["CAI", "Thomas Struszer", "P", "P", "P", "P", "P"],
+    #     ["CAI", "Agustín Cox", "P", "P", "P", "P", "P"],
+    #     ["CAI", "Claudio Escobar", "P", "P", "P", "P", "P"],
+    #     ["CAI", "Javiera Dawabe", "P", "P", "P", "P", "P"],
+    #     ["CT", "José Pereira", "P", "P", "P", "A", "P"],
+    #     ["CT", "Trinidad Larraín", "P", "P", "P", "P", "A"],
+    #     ["CT", "María Ignacia Henriquez", "P", "P", "P", "J", "P"],
+    #     ["CT", "Tomás Álvarez", "A", "A", "A", "A", "A"],
+    #     ["CT", "Magdalena Merino", "P", "P", "P", "P", "P"],
+    # ]
 
     # academico = get_academic(service, "1ditHP6pQUyAx73t_76csuJUH4t4TPqlRa5CHHWRXz3o")
 
