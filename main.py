78907044ad60b8944e3f477e8e67dde73702b4dd
Agustín Covarrubias <agucova@uc.cl>
2020-08-10 23:04:32 -0400

    Add new CORS source (new website)

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, use_colors=True)
 
 origins = [
+    "https://ct.agucova.me"
     "https://agucova.github.io",
     "http://localhost",
     "http://127.0.0.1",
